5c614cd60f03a5cdd805908d8cfd4ed1049d6328
Raghunandhan AJ <raghunandhanaj@Raghunandhans-MacBook-Pro.local>
2025-04-25 10:24:29 +0530

    Add Maps component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import CustomMap from './CustomMap';
+import { APIProvider } from "@vis.gl/react-google-maps";
 
 function App() {
+  console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <APIProvider>
+    <CustomMap apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}/>
+    </APIProvider>
   );
 }
 
